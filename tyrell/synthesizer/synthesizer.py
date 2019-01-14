c0839550435447b7cd3a4e29e076f40cfac45828
Jia Chen <grievejia@gmail.com>
2019-01-13 18:28:25 -0600

    Fixed a missing import in synthesizer

--- tyrell/synthesizer/synthesizer.py
+++ tyrell/synthesizer/synthesizer.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import Any
+from ..interpreter import InterpreterError
 from ..enumerator import Enumerator
 from ..decider import Decider
 from ..dsl import Node
